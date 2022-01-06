92cede0aca7f68e95d684a6c6dd8e071311d3374
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-06 13:55:11 +0300

    initial commit

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,9 @@
+const Preloader = () => {
+    return (
+        <div className='progress'>
+            <div className='indeterminate deep-purple accent-4'></div>
+        </div>
+    );
+}
+
+export default Preloader;
