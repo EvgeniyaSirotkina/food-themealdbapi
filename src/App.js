860aafb9e6152ffa36536ca57d51d166df96433e
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-06 15:25:39 +0300

    add routing in to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-function App() {
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+export default function App() {
   return (
     <>
       <Header />
+      <main className='container content'>
+        <Router>
+          <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </Router>
+      </main>
       <Footer />
     </>
   );
 }
-
-export default App;
