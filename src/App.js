dfe069392a0561af66a8744a4931660578f1d73a
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-06 19:49:13 +0300

    add NotFound page as default

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
               <Route exact path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
               <Route path='/contact' element={<Contact />} />
+              <Route path='*' element={<NotFound />} />
           </Routes>
         </Router>
       </main>
