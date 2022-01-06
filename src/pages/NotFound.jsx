8aca86bdaaed01f905455803a8d1fc6658e2a727
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-06 19:48:52 +0300

    move NotFound into pages folder

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+    return (
+        <div className='row'>
+            <div className='card deep-purple accent-1'>
+                <div className='card-content black-text'>
+                    <span className='card-title'>Not Found!</span>
+                    <p>Something went wrong</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
