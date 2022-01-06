e216ef6feca40cac389374c5fb0602a879605deb
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-06 22:14:14 +0300

    update main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+import { getAllMealCategories } from '../api'
+
+import Preloader from '../components/Preloader';
+import CategoryList from '../components/CategoryList';
+
 const Home = () => {
+    const [catalog, setCatalog] = useState([]);
+
+    useEffect(() => {
+        getAllMealCategories().then(data => {
+            setCatalog(data.categories);
+        })
+            .catch(error => console.dir(error));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <>
-            <h3>Home page</h3>
+            {
+                !catalog.length ? <Preloader /> : <CategoryList catalog={catalog} />
+            }
         </>
     );
 }
