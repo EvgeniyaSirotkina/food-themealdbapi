e68420617c93d32ecc8eabdc5cadadeede86ae74
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-06 21:16:37 +0300

    remove link on git hubf from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,8 @@ const Header = () => {
     return (
         <nav className='deep-purple accent-3'>
             <div className='nav-wrapper'>
-            <Link to={'/food-themealdbapi'} className='brand-logo right'>React Shop</Link>
+                <Link to={'/food-themealdbapi'} className='brand-logo right'>React Shop</Link>
                 <ul id='nav-mobile' className='left hide-on-med-and-down'>
-                    <li>
-                        <a
-                            href='https://github.com/EvgeniyaSirotkina?tab=repositories'
-                            target='_blank'
-                            rel='noreferrer'
-                        >
-                            GitHub
-                        </a>
-                    </li>
                     <li><Link to={'/food-themealdbapi/about'}>About</Link></li>
                     <li><Link to={'/food-themealdbapi/contacts'}>Contacts</Link></li>
                 </ul>
